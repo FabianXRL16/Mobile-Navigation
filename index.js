d17c163eff36637067977df6670dd53dd96c6d27
Fabian Pacherres <fabianpacherres@gmail.com>
2021-12-06 22:40:13 -0500

    Animation dynamic with event click

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-alert("ok")
+const items = document.querySelectorAll(".items");
+function activeItem() {
+  items.forEach((item) => item.classList.remove("active"));
+  this.classList.add("active");
+}
+items.forEach((item) => item.addEventListener("click", activeItem));
